83abe5cc64876ffdce1e4778c40211adca748faa
jbsantos <jorge0302@gmail.com>
2021-05-28 11:18:29 -0300

    host do heroku

--- routes.py
+++ routes.py
@@ -29,5 +29,5 @@ def dados():
     #return render_template('template.html',classe='0')
 
 if __name__ == "__main__":
-#        port = int(os.environ.get('PORT', 5000))
- #       app.run(host='0.0.0.0', port=port)
+         port = int(os.environ.get('PORT', 5000))
+         app.run(host= https://grafico-python.herokuapp.com', port=port)
